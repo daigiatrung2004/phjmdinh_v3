ae20fde469ac359f62f21cd65918b0cb678a4275
daigiatrung2004 <55388468+daigiatrung2004@users.noreply.github.com>
2022-09-25 11:40:51 +0700

    initialize base

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import { createStore } from 'redux';
+
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<button type="button" className="btn btn-default" id = "increment" >increment</button>
+				<div className="result" id = "result">0</div>
+				<button type="button" className="btn btn-default" id ="decrement">decrement</button>
+			</header>
+		</div>
+	);
+}
+
+export default App;
