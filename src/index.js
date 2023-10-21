41829da4c2320c19c5733cc29a1d8d0b91486d19
vu dao <linkthuy9@gmail.com>
2023-10-21 19:21:40 +0700

    1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '~/App';
+import App from '~/pages/TestPopUp';
 import GlobalStyles from '~/components/Globalstyles';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+
+const router = createHashRouter([
+	{
+		path: '/*',
+		element: <App />,
+	},
+]);
 
 ReactDOM.render(
+	<RouterProvider router={router} />,
 	// <React.StrictMode>
-	<GlobalStyles>
-		<App />
-	</GlobalStyles>,
+	// <GlobalStyles>
+	// 	<App />
+	// </GlobalStyles>,
 	// </React.StrictMode>,
 	document.getElementById('root')
 );
