61590e44cc29273c2bfa837c5c4bdd01b25d5870
vu dao <linkthuy9@gmail.com>
2023-10-21 19:24:37 +0700

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '~/pages/TestPopUp';
-import GlobalStyles from '~/components/Globalstyles';
+// import GlobalStyles from '~/components/Globalstyles';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
 
 const router = createHashRouter([
